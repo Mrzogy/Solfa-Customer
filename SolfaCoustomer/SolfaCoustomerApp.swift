18cfa441e8ad057992e0c2e183069d7de68c6f51
mrzogy <magbolmrzogy@gmail.com>
2023-06-15 09:54:50 +0300

    Initial Commit

---
+++ SolfaCoustomer/SolfaCoustomerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolfaCoustomerApp.swift
+//  SolfaCoustomer
+//
+//  Created by 3bood on 26/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SolfaCoustomerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
